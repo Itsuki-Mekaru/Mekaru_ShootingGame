82ecfba943899ebf47667f38e0d42a3436b12d01
銘苅　一輝 <i-mekaru@kbcgroup.ac.jp>
2022-10-18 13:07:37 +0900

    玉をうつようにした

--- Mekaru_ShootingGame/Mekaru_ShootingGame/Player.cpp
+++ Mekaru_ShootingGame/Mekaru_ShootingGame/Player.cpp
@@ -1,26 +1,67 @@
 #include "DxLib.h"
 #include "Player.h"
+#include "KeyManager.h"
+#include "StraightBullets.h"
+
+T_Location getNewLocation(T_Location newLocation);
 
 Player::Player(T_Location location, float radius) 
     : SphereCollider(location, radius)
 {
     score = 0;  // åªç›ÉXÉRÉA
-    life = 10;   // HP
+    life = 10;  // HP
     // imageÇÃèâä˙âª
     // speedÇÃèâä˙âª
+
+    bullets = new BulletsBase*[30];
+    for(int i = 0; i < 30; i++)
+    {
+        bullets[i] = nullptr;
+    }
 }
 
 void Player::Update()
 {
-    T_Location l;
-    l.x = GetLocation().x + 0.1f;
-    l.y = GetLocation().y;
-    SetLocation(l);
+    T_Location newLocation = getNewLocation(GetLocation());
+    SetLocation(newLocation);
+
+    int bulletCount;
+    for(bulletCount = 0; bulletCount < 30; bulletCount++)
+    {
+        if(bullets[bulletCount] == nullptr)
+        {
+            break;
+        }
+        bullets[bulletCount]->Update();
+    }
+
+    if(KeyManager::OnClick(KEY_INPUT_P))
+    {
+        int i;
+        for(i = 0; i < 30; i++)
+        {
+            if(bullets[i] == nullptr)
+            {
+                break;
+            }
+        }
+        bullets[i] = new StraightBullets(GetLocation());
+    }
 }
 
 void Player::Draw()
 {
-    DrawCircle(GetLocation().x, GetLocation().y, GetRadius(), GetColor(0, 0, 255));
+    DrawCircle(GetLocation().x, GetLocation().y, GetRadius(), GetColor(255, 0, 0));
+
+    int bulletCount;
+    for(bulletCount = 0; bulletCount < 30; bulletCount++)
+    {
+        if(bullets[bulletCount] == nullptr)
+        {
+            break;
+        }
+        bullets[bulletCount]->Draw();
+    }
 }
 
 void Player::Hit()
@@ -36,4 +77,25 @@ bool Player::LifeCheck()
 int Player::GetScore()
 {
     return score;
+}
+
+T_Location getNewLocation(T_Location newLocation)
+{
+    if(KeyManager::OnPressed(KEY_INPUT_W))
+    {
+        newLocation.y -= 2;
+    }
+    if(KeyManager::OnPressed(KEY_INPUT_A))
+    {
+        newLocation.x -= 2;
+    }
+    if(KeyManager::OnPressed(KEY_INPUT_S))
+    {
+        newLocation.y += 2;
+    }
+    if(KeyManager::OnPressed(KEY_INPUT_D))
+    {
+        newLocation.x += 2;
+    }
+    return newLocation;
 }
---
+++ Mekaru_ShootingGame/Mekaru_ShootingGame/StraightBullets.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "BulletsBase.h"
+class StraightBullets : public BulletsBase
+{
+private:
+    int image;  // 一旦放置
+
+public:
+    StraightBullets(T_Location location);  // コンストラクタ
+
+public:
+    virtual void Update() override;
+    virtual void Draw() override;
+};
+
